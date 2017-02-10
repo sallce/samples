a3965787308df3b4b427a9cdb8d214d8130d641f
Yachao Shang <yachaocs@gmail.com>
2017-02-10 15:51:10 -0500

    Optimize move word

--- 2.6/jsscript.js
+++ 2.6/jsscript.js
@@ -1,37 +1,31 @@
-var left_box = document.getElementById('left_box');
-var right_box = document.getElementById('right_box');
 var counter = 0;
-var interval_to_right;
-var interval_to_left;
+var interval_to;
 
-function to_right_clicked(){
-
-  clearInterval(interval_to_left);
-  interval_to_right = setInterval(function(){
-    var left_len = left_box.value.length;
-    var right_len = right_box.value.length;
-    if(left_len > 0){
-      right_box.value = right_box.value + left_box.value[0];
-      left_box.value = left_box.value.slice(1, left_len);
-    }
-  },1000);
-
-}
-
-function to_left_clicked(){
-  clearInterval(interval_to_right);
-  interval_to_left = setInterval(function(){
-    var left_len = left_box.value.length;
-    var right_len = right_box.value.length;
-    if(right_len > 0){
-      left_box.value = right_box.value[right_len-1] + left_box.value;
-      right_box.value = right_box.value.slice(0, right_len-1);
-    }
-  },1000);
-
-}
-
-function pause_clicked(){
-  clearInterval(interval_to_left);
-  clearInterval(interval_to_right);
+function button_clicked(direction){
+  switch (direction) {
+    case 'to_left':
+      clearInterval(interval_to);
+      interval_to = setInterval(function(){
+        var left_len = left_box.value.length;
+        var right_len = right_box.value.length;
+        if(right_len > 0){
+          left_box.value = right_box.value[right_len-1] + left_box.value;
+          right_box.value = right_box.value.slice(0, right_len-1);
+        }
+      },1000);
+      break;
+    case 'to_right':
+      clearInterval(interval_to);
+      interval_to = setInterval(function(){
+        var left_len = left_box.value.length;
+        var right_len = right_box.value.length;
+        if(left_len > 0){
+          right_box.value = right_box.value + left_box.value[0];
+          left_box.value = left_box.value.slice(1, left_len);
+        }
+      },1000);
+      break;
+    default:
+      clearInterval(interval_to);
+  }
 }
